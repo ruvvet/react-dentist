48bf3cec098c9fe2c5a6e2eee3a67673bc080cf8
Jenny <finej01@gmail.com>
2020-12-11 11:18:10 -0500

    update

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+
+export default function Home() {
+  return (
+    <>
+      <ul>
+        <li><Link to="/">home</Link></li>
+        <li><Link to="/contact">contact</Link></li>
+        <li><Link to="/procedures"></Link></li>
+      </ul>
+    </>
+  );
+}
